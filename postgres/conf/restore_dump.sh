b1f5dffb4fb1c846f04bf935013c598cfb7ab4ce
aburgosbcn <aburgos@bcnschool.cl>
2024-07-11 00:34:51 -0400

    Initial Commit

---
+++ postgres/conf/restore_dump.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+if [ -e "/opt/db_dumps/dump-init.dump" ]; then
+    pg_restore -U ${POSTGRES_USER} -d postgres -c -C -O --role ${POSTGRES_USER} /opt/db_dumps/dump-init.dump
+fi
---
+++ test/scripts/install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Fail if one of the commands of this script fails
+set -e
+
+git clone https://github.com/moodle/moodle.git html
+
+set +e
